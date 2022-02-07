f2f025c89584e95bd6029c112e7c673d2b253c9e
xuanxuan <xuanandjava@163.com>
2022-02-07 10:26:53 +0800

    \初级算法\数组\移动零

--- src/初级算法/数组/移动零/MobileZero.java
+++ src/初级算法/数组/移动零/MobileZero.java
@@ -10,8 +10,41 @@ public class MobileZero {
     public static void main(String[] args) {
         MobileZero mobileZero = new MobileZero();
         int[] ints = new int[]{0,1,0,3,12};
-        mobileZero.moveZeroes2(ints);
+//        mobileZero.moveZeroes2(ints);
+        mobileZero.moveZeroes3(ints);
         System.out.println(Arrays.toString(ints));
+    }
+
+    /**
+     * 倒叙，（不能保证元素顺序，无法通过）
+     */
+    public void moveZeroes3(int[] nums) {
+        int exchangeIndex = nums.length-1;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] == 0){
+                if (nums[i] == 0){
+                    int num = nums[exchangeIndex];
+                    nums[exchangeIndex] = nums[i];
+                    nums[i] = num;
+                    exchangeIndex--;
+                }
+            }
+
+        }
+    }
+
+    /** 双指针的解法 */
+    public void moveZeroes2(int[] nums) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0){
+                int num = nums[index];
+                nums[index] = nums[i];
+                nums[i] = num;
+                index++;
+            }
+        }
     }
 
     public void moveZeroes(int[] nums) {
@@ -31,11 +64,4 @@ public class MobileZero {
         }
     }
 
-    public void moveZeroes2(int[] nums) {
-        int i = nums.length - 1;
-        for (int index = nums.length - 1; index >= 0; index--) {
-            if ()
-        }
-
-    }
 }
